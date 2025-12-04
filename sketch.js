112e4155d03b95f3cae66796166c856a3b9cfc6e
Anayo <mark.agwunobi@gmail.com>
2025-12-04 16:55:43 +0000

    Drawn seed cell on canvas.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
+const tree = [];
+const radius = 16;
+
 function setup() {
   createCanvas(400, 400);
+  tree[0] = createVector(width/2, height/2); // point in the middle of the window
+  let walker = createVector(random, random)
 }
 
 function draw() {
-  background(220);
+  background(0);
+
+  
+
+  for (let i = 0; i < tree.length; i++)
+  {
+    strokeWeight(radius);
+    stroke(255);
+    point(tree[i].x, tree[i].y); 
+  }
 }
